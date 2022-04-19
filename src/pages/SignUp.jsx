ae92104aa68459238f2dd153540f35950202d2bd
Juan Pablo Bedoya Mosquera <bedojuanpablo@gmail.com>
2022-04-18 23:15:22 -0500

    18-04-2022 - 3

--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -74,7 +74,7 @@ const SignUp = () => {
 
   return (
     <>
-      <h1 className="text-sky-600 font-black text-lg md:text-2xl lg:text-6xl capitalize">Crea tu cuenta y administra tus {''}<span className="text-slate-700">proyectos</span></h1>
+      <h1 className="text-sky-600 font-black text-2xl md:text-4xl lg:text-6xl capitalize">Crea tu cuenta y administra tus {''}<span className="text-slate-700">proyectos</span></h1>
       <form className="my-10 bg-white shadow rounded-lg p-10" onSubmit={handleSubmit}>
         <div className="my-5">
           <label htmlFor="name" className="uppercase text-gray-600 block text-xl font-bold">Nombre</label>
